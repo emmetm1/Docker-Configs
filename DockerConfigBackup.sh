fd57386ad38c576f25aeea76fa8ed2f855c4dfa3
emmetm1 <emmetm1@gmail.com>
2023-03-22 12:32:11 -0500

    backup paths updated to reflect changes

--- DockerConfigBackup.sh
+++ DockerConfigBackup.sh
@@ -5,11 +5,18 @@
 ## Format date ##
 today=$(date +"%Y-%m-%d")
 
+
 ## Stop containers ##
-docker-compose -f /home/Docker/MediaCompose.yml down
+docker compose -f /home/emmet/docker/docker-compose.yaml down
 
 ## Tar current config to shared drive ##
-tar --create --preserve-permissions --xz --file=/mnt/share/DockerConfigs/${today}.tar.xz /home/Docker
+if [ $? -eq 0 ]
+then
+    tar --create --preserve-permissions --xz --file=/mnt/WhemFiles/DockerConfigs/${today}.tar.xz /home/emmet/docker
+else
+    touch /mnt/WhemFiles/DockerConfigs/BACKUPFAILED_${today}.txt
+fi
+
 
 ## Restart containers ##
-docker-compose -f /home/Docker/MediaCompose.yml up -d
+docker compose -f /home/emmet/docker/docker-compose.yaml up -d
